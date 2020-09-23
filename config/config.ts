6f85cc5831d982748f7f0a83ab5c4128df7d3495
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2020-09-22 15:05:48 -1000

    initial commit

---
+++ config/config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+export const FirebaseConfig = {
+    apiKey: "AIzaSyCuA5AGGJEilOGt0YdkFBjoSrBTg3iqCqA",
+    authDomain: "fir-storage-e3592.firebaseapp.com",
+    databaseURL: "https://fir-storage-e3592.firebaseio.com",
+    projectId: "fir-storage-e3592",
+    storageBucket: "fir-storage-e3592.appspot.com",
+    messagingSenderId: "977673846182",
+    appId: "1:977673846182:web:006b7cab73a9a4c69f6367"
+  };
+
+  
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// import { FirebaseConfig } from '../config/config';
+import {baz} from '../foo/bar'
+ baz();
+import * as firebase from 'firebase/app';
+// const initialize = firebase.initializeApp(FirebaseConfig);
