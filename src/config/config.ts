48e395f2a84c7945c879b6f30bf31e6873a11e45
Anthony Yarbrough <asyarb@hawaii.edu>
2020-09-27 16:32:38 -1000

    feat: setup esm with TS

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+export const FirebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-db-url",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-id",
+  appId: "your-api-key",
+}
